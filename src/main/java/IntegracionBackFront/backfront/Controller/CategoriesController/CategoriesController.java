d990a1eb289c2604c9442515846510c33852b715
AbelVJ <oscar.joyar@gmail.com>
2025-07-31 14:07:16 -0600

    Cambios

---
+++ src/main/java/IntegracionBackFront/backfront/Controller/CategoriesController/CategoriesController.java
@@ -0,0 +1,9 @@
+package IntegracionBackFront.backfront.Controller.CategoriesController;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("apiUser")
+public class CategoriesController {
+}
